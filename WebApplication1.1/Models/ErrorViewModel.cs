a9286771274e0c8f868d86f2391bcf04bebb5153
RAFAELI CARVALHO <rafaeli.carvalho@fatec.sp.gov.br>
2023-05-25 10:05:46 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1.1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1._1_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
